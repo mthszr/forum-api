3a7f18a09ee6a0544aefa4f0c327063f597baaed
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-07 14:51:22 -0300

    feat: add Slug value object and integrate it into Question entity; include tests for Slug creation

---
+++ src/domain/entities/value-objetcs/slug.ts
@@ -0,0 +1,29 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string of text and normalizes it to create a slug.
+   *
+   * Example: "Hello, World!" -> "hello-world"
+   *
+   * @param text {string}
+   */
+
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+}
