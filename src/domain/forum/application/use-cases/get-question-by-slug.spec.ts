81a17a89350e49bf1c0dfcf400098983dbe42649
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-08 10:34:30 -0300

    feat: add @faker-js/faker for test data generation and update related tests

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,6 +1,5 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { Question } from '../../enterprise/entities/question'
 import { Slug } from '../../enterprise/entities/value-objetcs/slug'
 import { GetQuestionBySlug } from './get-question-by-slug'
 
@@ -14,11 +13,8 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to get a question by slug ', async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityID('author-id'),
-      title: 'Any Title',
+    const newQuestion = makeQuestion({
       slug: Slug.create('any-title'),
-      content: 'Any Content',
     })
 
     await inMemoryQuestionsRepository.create(newQuestion)
