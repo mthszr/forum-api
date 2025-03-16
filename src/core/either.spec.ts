808d8129a2f5355afcb144dfc18b4e3392969d3e
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-16 16:46:27 -0300

    feat: enhance Either type to support error handling with Left and Right classes

---
+++ src/core/either.spec.ts
@@ -0,0 +1,24 @@
+import { type Either, left, Right, right } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(1)
+  } 
+  
+  return left('error')
+  
+}
+
+test('success result', () => {
+  const result = doSomething(true)
+
+  expect(result.isRight()).toBe(true)
+  expect(result.isLeft()).toBe(false)
+})
+
+test('error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isLeft()).toBe(true)
+  expect(result.isRight()).toBe(false)
+})
