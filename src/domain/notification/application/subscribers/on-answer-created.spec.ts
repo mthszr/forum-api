ef6f2a6f2d4c1a5810367f8cae2566cc599c44e2
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-18 19:18:50 -0300

    implement answer creation event and notification system; enhance Answer class with domain event capabilities

---
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -0,0 +1,22 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+
+describe('On Answer Created', () => {
+  beforeEach(() => {
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository
+    )
+  })
+
+  it('should send a notification when an answer is created', () => {
+    const answer = makeAnswer()
+
+    inMemoryAnswersRepository.create(answer)
+  })
+})
