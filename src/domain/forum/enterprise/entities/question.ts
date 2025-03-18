2082baedbdd794c56ff3cb1381dc377294e67146
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-18 08:59:15 -0300

    feat: introduce AggregateRoot class and refactor entities to extend it

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,7 @@
-import dayjs from 'dayjs'
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root' // Ensure AggregateRoot is a class and properly exported
 import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
 import { Slug } from './value-objetcs/slug'
 
 export interface QuestionProps {
@@ -14,7 +14,7 @@ export interface QuestionProps {
   updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId
   }
